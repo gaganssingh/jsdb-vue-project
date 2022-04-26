1a74f9a74bd059f86a2e1257716c716b9dbafdb6
Gagan Singh <gagansingh.dev@outlook.com>
2022-04-25 20:28:32 -0400

    Implemented nav menu items and tests

--- test/unit/components/MainNav.test.js
+++ test/unit/components/MainNav.test.js
@@ -6,4 +6,20 @@ describe(`MainNav`, () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Career Maker");
   });
+
+  it(`displays navigation menu items`, () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const menus = navigationMenuItems.map((item) => item.text());
+    expect(menus).toEqual([
+      "Teams",
+      "Locations",
+      "Life at CM",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
